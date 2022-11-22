9aa8dab673324004e7ef6d456a8a92967feaccac
Diogenes-Leal <diogenesferraz10@hotmail.com>
2022-11-22 16:50:21 -0300

    Função de criar itens feita

--- script/criar-item.js
+++ script/criar-item.js
@@ -1,4 +1,37 @@
 // Pegar todos os elementos que estão no HTML necessários para executar a função
-const btnCriarItem = document.getElementById('btn-criar-item');
+const listaDeTarefas = document.getElementById('lista-de-tarefas');
 const textDescricaoItem = document.getElementById('descrever-item');
+const classeDaDiv = 'list-item';
+const classeDoLabel = 'lbl-list-item'
 
+
+function criarItem(){
+    // Elementos para colocar na lista de tarefa
+    const descricaoItem = textDescricaoItem.value != '' ? textDescricaoItem.value:null;
+
+    if(descricaoItem != null){
+        const novaDiv = document.createElement('div');
+        const novoCheck = document.createElement('input');
+        const novoLabel = document.createElement('label');
+        
+        novaDiv.className = classeDaDiv;
+
+        novoCheck.type = 'checkbox';
+        novoCheck.name = descricaoItem;
+        novoCheck.id = descricaoItem;
+        
+        novoLabel.htmlFor = descricaoItem;
+        novoLabel.className = classeDoLabel;
+        novoLabel.appendChild(document.createTextNode(descricaoItem));
+        
+        novaDiv.appendChild(novoCheck);
+        novaDiv.appendChild(novoLabel);
+        
+        listaDeTarefas.appendChild(novaDiv);
+        limparInputDescricaoItem();
+    }
+}
+
+function limparInputDescricaoItem(){
+    textDescricaoItem.value = '';
+}
